3c52d29bb05c660375e47e235e1525199532d34f
Owen <owenowen@Owens-MBP.hitronhub.home>
2018-07-18 13:08:29 -0600

    2nd commit

---
+++ apps/jenk/djenk/src/test/java/com/ojw/djenk/DjenkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ojw.djenk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DjenkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
